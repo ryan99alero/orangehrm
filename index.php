22ef94ef7c54d5fb0d146870b984de30db6de24c
Ryan99alero <ryan@randgraphics.com>
2023-12-08 21:44:19 -0600

    Who knows

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ use OrangeHRM\Config\Config;
 require realpath(__DIR__ . '/src/vendor/autoload.php');
 
 /* For logging PHP errors */
-/*include_once('./src/config/log_settings.php');*/
+include_once('./src/config/log_settings.php');
 
 if (!Config::isInstalled()) {
     header('Location: ./installer/index.php');
