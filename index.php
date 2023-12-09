c8268639c7605751b9d636afcfa2351ce87ed68f
Ryan99alero <ryan@randgraphics.com>
2023-12-08 21:07:37 -0600

    Who knows

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 /**
  * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
  * all the essential functionalities required for any enterprise.
